0598130dc851adb66514e71183d2d8ef893b27fd
Erik Rodriguez <perhapserik@gmail.com>
2021-06-11 15:56:25 -0700

    • Adds comments to index and server files

--- Api/server.js
+++ Api/server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
+// api instance
 const server = express();
 
+// security
 server.use(helmet());
+
+// resource access
 server.use(cors());
 
 server.get("/", (req, res) => {
